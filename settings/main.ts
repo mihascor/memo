d12ac073ecdda2180a0a1ef73148b28c433ff04f
mihascor <mihascorkiz@gmail.com>
2026-03-22 12:38:49 +0300

    Лого_3

--- settings/main.ts
+++ settings/main.ts
@@ -8,7 +8,7 @@ export const sitename = 'Памятка'
 export const description =
   'Инструкция для проживающих на планете Земля. Небольшие хитрости для оптимального обустройства и комфорта.'
 export const keywords = ['Разум', 'Осознанность', 'Понимание', 'Принятие', 'mihascor']
-export const urlimage = '/images/og-image_v2.png'
+export const urlimage = '/images/og-image.png'
 export const imagealt = 'mihascor'
 export const twitterhandle = ''
 
